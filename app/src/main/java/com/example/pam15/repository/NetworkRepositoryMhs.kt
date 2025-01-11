32270ed96de699e53fcab8eeaf3fada4851df5db
nrismawardah <nrismawardah@gmail.com>
2025-01-11 20:21:44 +0700

    Melengkapkan fungsi insertMhs

--- app/src/main/java/com/example/pam15/repository/NetworkRepositoryMhs.kt
+++ app/src/main/java/com/example/pam15/repository/NetworkRepositoryMhs.kt
@@ -6,13 +6,18 @@ import com.google.firebase.firestore.Query
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.tasks.await
 
 class NetworkRepositoryMhs(
     private val firestore: FirebaseFirestore
 ) : RepositoryMhs {
 
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
+        try {
+            firestore.collection("Mahasiswa").add(mahasiswa).await()
+        } catch (e: Exception) {
+            throw Exception("Gagal menambahkan data mahasiswa:${e.message}")
+        }
     }
 
     override fun getAllMhs(): Flow<List<Mahasiswa>> = callbackFlow {
