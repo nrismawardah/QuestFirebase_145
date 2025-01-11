eda30ed02430c1688032282b373b2f553b2050c3
nrismawardah <nrismawardah@gmail.com>
2025-01-11 20:50:32 +0700

    Membuat data class untuk MahasiswaEvent, FormErrorState, InsertUiState

---
+++ app/src/main/java/com/example/pam15/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.pam15.ui.viewmodel
+
+data class InsertUiState(
+    val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isEntryValid: FormErrorState = FormErrorState(),
+)
+
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val jenisKelamin: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null
+) {
+    fun isValid(): Boolean {
+        return nim == null && nama == null && jenisKelamin == null &&
+                alamat == null && kelas == null && angkatan == null
+    }
+}
+
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
