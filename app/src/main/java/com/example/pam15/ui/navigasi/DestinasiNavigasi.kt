1f7b15987c933d73dbc5432918d70663858a27e1
nrismawardah <nrismawardah@gmail.com>
2025-01-11 22:05:03 +0700

    Membuat interface DestinasiNavigasi, object DestinasiHome dan object DestinasiInsert

---
+++ app/src/main/java/com/example/pam15/ui/navigasi/DestinasiNavigasi.kt
@@ -0,0 +1,15 @@
+package com.example.pam15.ui.navigasi
+
+interface DestinasiNavigasi {
+    val route: String
+    val titleRes: String
+}
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route: String = "home"
+    override val titleRes: String = "Home"
+}
+object DestinasiInsert : DestinasiNavigasi {
+    override val route: String = "insert"
+    override val titleRes: String = "Insert"
+}
