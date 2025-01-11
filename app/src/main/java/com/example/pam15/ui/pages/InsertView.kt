ab40e27279d4fdb1a5f888d7675be03274ac43c5
nrismawardah <nrismawardah@gmail.com>
2025-01-11 21:51:54 +0700

    Membuat tampilan InsertBodyMh pada InsertView

--- app/src/main/java/com/example/pam15/ui/pages/InsertView.kt
+++ app/src/main/java/com/example/pam15/ui/pages/InsertView.kt
@@ -6,7 +6,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
@@ -17,7 +21,49 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.example.pam15.ui.viewmodel.FormErrorState
+import com.example.pam15.ui.viewmodel.FormState
+import com.example.pam15.ui.viewmodel.InsertUiState
 import com.example.pam15.ui.viewmodel.MahasiswaEvent
+
+@Composable
+fun InsertBodyMhs(
+    modifier: Modifier = Modifier,
+    onValueChange: (MahasiswaEvent) -> Unit,
+    uiState: InsertUiState,
+    onClick: () -> Unit,
+    homeUiState: FormState
+){
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        FormMahasiswa(
+            mahasiswaEvent = uiState.insertUiEvent,
+            onValueChange = onValueChange,
+            errorState = uiState.isEntryValid,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onClick,
+            modifier = Modifier.fillMaxWidth(),
+            enabled = homeUiState !is FormState.Loading,
+        ){
+            if (homeUiState is FormState.Loading) {
+                CircularProgressIndicator(
+                    color = Color.White,
+                    modifier = Modifier
+                    .size(20.dp)
+                    .padding(end = 8.dp)
+            )
+                Text("Loading...")
+            } else {
+                Text("Add")
+            }
+        }
+    }
+}
+
 
 @Composable
 fun FormMahasiswa(
