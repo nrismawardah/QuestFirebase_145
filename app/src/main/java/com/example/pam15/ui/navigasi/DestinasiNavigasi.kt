0b00fb1edf3928acd642bd79bf648f1a31ba06e5
nrismawardah <nrismawardah@gmail.com>
2025-01-13 00:48:37 +0700

    Menambahkan object DestinasiDetail pada navigasi

--- app/src/main/java/com/example/pam15/ui/navigasi/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/pam15/ui/navigasi/DestinasiNavigasi.kt
@@ -9,7 +9,15 @@ object DestinasiHome : DestinasiNavigasi {
     override val route: String = "home"
     override val titleRes: String = "Home"
 }
+
 object DestinasiInsert : DestinasiNavigasi {
     override val route: String = "insert"
     override val titleRes: String = "Insert"
+}
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route: String = "detail"
+    override val titleRes: String = "Detail"
+    const val NIM = "nim"
+    val routesWithArg = "$route/{$NIM}"
 }
