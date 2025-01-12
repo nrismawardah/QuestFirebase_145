5dc77790c3f37b338b7c164cc6c14cd89aec25f9
nrismawardah <nrismawardah@gmail.com>
2025-01-13 00:45:32 +0700

    Membuat tampilan untuk ComponentDetail pada DetailView

---
+++ app/src/main/java/com/example/pam15/ui/pages/DetailView.kt
@@ -0,0 +1,34 @@
+package com.example.pam15.ui.pages
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ComponentDetail(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya, fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
