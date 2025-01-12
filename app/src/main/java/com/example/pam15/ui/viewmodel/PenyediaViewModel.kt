64c8af650a3a7435341eebe27ddb42ab9afee669
nrismawardah <nrismawardah@gmail.com>
2025-01-13 00:43:38 +0700

    Mendaftarkan DetailViewModel di PenyediaViewModel

--- app/src/main/java/com/example/pam15/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pam15/ui/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.pam15.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -10,6 +11,7 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(mahasiswaApp().containerApp.repositoryMhs) }
         initializer { InsertViewModel(mahasiswaApp().containerApp.repositoryMhs) }
+        initializer { DetailViewModel(createSavedStateHandle(),mahasiswaApp().containerApp.repositoryMhs) }
     }
 }
 
